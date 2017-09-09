00911a82528d43a57b59efe34df5c9c38f560b68
Reid <reid.t.sata@gmail.com>
2017-09-09 12:36:46 -0700

    combine into one request

--- server/db.js
+++ server/db.js
@@ -1,26 +1,25 @@
 let MongoClient = require('mongodb').MongoClient;
 let assert = require('assert');
 let moment = require('moment');
+let fs = require('fs');
+let plaid = require('plaid');
+let secrets = require('../secrets');
+
+let plaidClient = new plaid.Client(secrets.secrets.plaid_client_id, secrets.secrets.plaid_secret, secrets.secrets.plaid_public_key, plaid.environments.development);
 
 // Connection URL
 var url = 'mongodb://localhost:27017/budget_db';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, (err, db) => {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  const collection = db.collection("saved_transactions");
-
-  collection.find({}).toArray((err, docs) => {
-    console.log(docs);
-  });
-
-  db.close();
-});
-
 const mongoQueries = {
-  getAll: (collection) => {
-    return collection.find().toArray();
+  getAll: (coll) => {
+    MongoClient.connect(url, (err, db) => {
+      assert.equal(null, err);
+      const collection = db.collection(coll);
+
+      return collection.find({}).toArray();
+
+      db.close();
+    });
   },
 
   insert: (collection, id, type) => {
@@ -39,5 +38,61 @@ const mongoQueries = {
     collection.updateOne({ transaction_id: id}, {$set: {type}});
   }
 };
+
+function q() {
+  MongoClient.connect(url, (err, db) => {
+    if(err) throw err;
+
+    let budgetProfile = 'budget_profiles';
+    let savedTransactions = 'saved_transactions';
+    Promise.all([
+      mongoPromise(budgetProfile, {user_id: 'reid'}),
+      mongoPromise(savedTransactions, {}),
+      plaidPromise()
+    ]).then(function(result) {
+      // result is an array of responses here
+      console.log('zzzzzzzzzzz', result);
+      db.close();
+    }).catch(function(err) {
+      console.log(err);
+      db.close();
+    });
+
+    function mongoPromise(collection, query) {
+      return new Promise(function(resolve, reject) {
+        db.collection(collection).find(query).toArray(function(err, resp) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(resp);
+          }
+        });
+      });
+    }
+
+    function plaidPromise(collection, query) {
+      return new Promise(function(resolve, reject) {
+        fs.readFile('./token', 'utf-8', (err, token) => {
+          let access_token_static = token.trim();
+          const now = moment();
+          const today = now.format('YYYY-MM-DD');
+          const startOfMonth = moment().startOf('month').format('YYYY-MM-DD');
+
+          plaidClient.getTransactions(access_token_static, startOfMonth, today, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
+          });
+        });
+      });
+    };
+  });
+}
+
+
+// q();
+
 
 module.exports = mongoQueries;
--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ let secrets = require('../secrets');
 let cookieParser = require('cookie-parser');
 let fs = require('fs');
 let moment = require('moment');
+let query = require('./db');
+let MongoClient = require('mongodb').MongoClient;
+let assert = require('assert');
+// let parallel = require('async/parallel');
+let async = require('async');
 
 app.use('/*', function (req, res, next) {
 
@@ -30,36 +35,87 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+// Connection URL
+var url = 'mongodb://localhost:27017/budget_db';
+
 let plaidClient = new plaid.Client(secrets.secrets.plaid_client_id, secrets.secrets.plaid_secret, secrets.secrets.plaid_public_key, plaid.environments.development);
 
-
-
 app.get('/transactions', (req, res) => {
-  // get transactions from plaid
-  fs.readFile('./token', 'utf-8', (err, token) => {
-    if (err) throw err;
-    let access_token_static = token.trim();
+  MongoClient.connect(url, (err, db) => {
+    if(err) throw err;
 
-    const now = moment();
-    const today = now.format('YYYY-MM-DD');
-    const startOfMonth = moment().startOf('month').format('YYYY-MM-DD');
-
-    plaidClient.getTransactions(access_token_static, startOfMonth, today, (err, data) => {
-      res.json({data, staticData: secrets.staticData});
+    Promise.all([
+      mongoPromise('budget_profiles', {user_id: 'reid'}),
+      mongoPromise('saved_transactions', {}),
+      plaidPromise()
+    ]).then(function(result) {
+      res.json(result);
+      db.close();
+    }).catch(function(err) {
+      console.log(err);
+      db.close();
     });
+
+    function mongoPromise(collection, query) {
+      return new Promise(function(resolve, reject) {
+        db.collection(collection).find(query).toArray(function(err, resp) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(resp);
+          }
+        });
+      });
+    }
+
+    function plaidPromise(collection, query) {
+      return new Promise(function(resolve, reject) {
+        fs.readFile('./token', 'utf-8', (err, token) => {
+          let access_token_static = token.trim();
+          const now = moment();
+          const today = now.format('YYYY-MM-DD');
+          const startOfMonth = moment().startOf('month').format('YYYY-MM-DD');
+
+          plaidClient.getTransactions(access_token_static, startOfMonth, today, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
+          });
+        });
+      });
+    };
   });
 });
 
 app.get('/saved_transactions', (req, res) => {
+  MongoClient.connect(url, (err, db) => {
+    assert.equal(null, err);
+    const collection = db.collection("saved_transactions");
 
+    collection.find({}).toArray((err, docs) => {
+      res.json(docs);
+    });
+
+    db.close();
+  });
 });
 
 app.get('/budget_profile', (req, res) => {
-  
+  MongoClient.connect(url, (err, db) => {
+    assert.equal(null, err);
+    const collection = db.collection("budget_profiles");
+
+    collection.find({}).toArray((err, docs) => {
+      res.json(docs);
+    });
+
+    db.close();
+  });
 });
 
 app.post('/plaid-auth', (req, res) => {
-  console.log(req.body.public_token);
   let public_token = req.body.public_token;
   plaidClient.exchangePublicToken(public_token, (err, exchangeTokenRes) => {
     if(err) {
